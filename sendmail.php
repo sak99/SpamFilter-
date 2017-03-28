a80cdfba04d94ffd809c369621f59da179e14a22
sak99 <sak99@users.noreply.github.com>
2017-03-28 21:52:30 +0530

    Delete sendmail.php

--- sendmail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "";
-$subject = "The mail subject goes here";
-$content= "And this is the mail content!";
-$headers = "From:Me@Mywebsite.Com\r\n";
-
-mail($to,$subject,$content,$headers);
-?>	
